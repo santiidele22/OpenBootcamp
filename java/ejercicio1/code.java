e16d8c16163196e44d46ccd16624933b215bf413
nikohud <nikohud86@gmail.com>
2022-06-23 12:22:54 -0300

    java

---
+++ java/ejercicio1/code.java
@@ -0,0 +1,53 @@
+package ejercicio1;
+public class code{
+public static void main(String[] args) {
+    var resultado = suma(10,20,30);
+    System.out.println(resultado);
+
+    Coche miCoche = new Coche();
+    miCoche.setDoors(4);
+    miCoche.setSpeedMax(220);
+    miCoche.setSwitchedOn(true);
+    System.out.println("la velocidad maxima del coche es:" + miCoche.getSpeedMax());
+    System.out.println("el coche esta encendido:?" + miCoche.getSwitchedOn());
+    System.out.println(miCoche.getDoors());
+}
+public static int suma(int a, int b, int c) {
+    return a + b + c;
+}
+}
+class Coche{
+    private int doors;
+    private int speedMax;
+    public boolean switchedOn = false;
+    //encendido /apagado
+    public void setSwitchedOn(boolean switchedOn){
+        this.switchedOn = switchedOn;
+    }
+    public boolean getSwitchedOn(){
+        return this.switchedOn;
+    } 
+    //velocidad
+    public void setSpeedMax(int speedMax){
+        this.speedMax = speedMax;
+    }
+    public int getSpeedMax(){
+        return this.speedMax;
+    }
+    //puertas
+    public void setDoors(int doors){
+        this.doors = doors;
+    }
+    public int getDoors(){
+        return this.doors;
+    }
+    public void pushDoors(){
+        this.doors++;
+    }
+    public void deleteDoors(){
+        this.doors--;
+    }
+
+ }
+    
+
---
+++ java/ejercicio2/code.java
@@ -0,0 +1,51 @@
+package ejercicio2;
+
+public class code {
+    public static void main(String[] args) {
+        var numeroIf = 10;
+        var numeroWhile = 1;
+        var numeroDo = 3;
+        var estacion = "verano";
+        //parte 1
+       if(numeroIf > 0){
+            System.out.println("Es positivo");
+        } else if (numeroIf < 0){
+            System.out.println("Es negativo");
+        } else {
+            System.out.println("Es cero");
+        } 
+        //parte 2
+        while(numeroWhile < 3){
+            System.out.println("number while:" + numeroWhile);
+            numeroWhile++;
+        }
+        //parte 3
+        do {
+            System.out.println("number do:" + numeroDo);
+            numeroDo++;
+        } 
+        while(numeroDo < 3);
+        //parte 4
+        for(var numberFor = 0; numberFor < 3; numberFor++){
+            System.out.println("numbers for:" + numberFor);
+        }
+        //ultima parte
+        switch(estacion){
+            case "verano":
+            System.out.println("es verano");
+            break;
+            case "invierno":
+            System.out.println("es invierno");
+            break;
+            case "otoño":
+            System.out.println("es otoño");
+            break;
+            case "primavera":
+            System.out.println("es primavera");
+            break;
+            default:
+            System.out.println(estacion);
+            break;
+        }
+    }
+}
