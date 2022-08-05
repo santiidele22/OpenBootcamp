db807e7e00ec64130ad525233051419a3af64b4b
santiidele22 <ezedelebecq22@gmail.com>
2022-08-05 14:21:12 -0300

    first commit

--- Ejercicios Javascripts/Ejercicio11/index.js
+++ Ejercicios Javascripts/Ejercicio11/index.js
@@ -3,13 +3,16 @@ const alwaysTrue = () => {
     return true;
   };
   //- Una función asíncrona que utilice un setTimeout y pase por consola un "Hola soy una promesa" 5 segundos después de haberse ejecutado
-  const async = () => {
+  /*const promesa = () => {
     return new Promise((resolve) => {
       setTimeout(() => {
         console.log(resolve("Hola soy una promesa"));
       }, 5000);
     });
-  };
+  }; */
+  async function promesa () {
+    return setTimeout(() => console.log("Hola soy una promesa"), 5000)
+  }
   //- Una función generadora de índices pares automáticos */
   function* idMaker(){
       var index = 0;
