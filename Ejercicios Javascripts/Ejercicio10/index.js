9e4b6dd12e044e448c68bb84bcaf28e79371ec6a
santiidele22 <ezedelebecq22@gmail.com>
2022-08-04 21:15:47 -0300

    first commit

---
+++ Ejercicios Javascripts/Ejercicio10/index.js
@@ -0,0 +1,14 @@
+/*- Abre una nueva ventana "about:blank" en Google Chrome
+
+- Abre la consola de desarrollador de Google Chrome (F12)
+
+- Pregunta al usuario cuál es su edad y almacénala en una variable llamada edad */
+const age = prompt("Cuantos años tenes")
+const over_18 = ( ) => {
+    if(age < 18){
+        return prompt("Sos menor de edad");
+    } else {
+        return prompt("Sos mayor de edad");
+    }
+}
+over_18();
