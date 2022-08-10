5a16f0745ddd108d9041dce3aca70bf16900892f
santiidele22 <ezedelebecq22@gmail.com>
2022-08-10 13:08:07 -0300

    first commit

---
+++ Ejercicios Javascripts/Ejercicio14/index.js
@@ -0,0 +1,26 @@
+/*Crea un nuevo fichero JS que contenga las siguientes líneas
+
+- Una clase llamada "Estudiante" que tenga:
+
+- Una variable llamada nombre
+
+- Otra variable lista llamada asignaturas con 3 asignaturas: Javascript, HTML, CSS
+
+- Un método "obtenDatos" que devuelva un objeto con las propiedades nombre y asignaturas
+
+- Crea una nueva instancia de "Estudiante"
+
+- Haz la llamada al método obtenDatos */
+class Estudiante {
+    name = 'santi'
+    asignaturas = ['html', 'css', 'javascript'];
+    obtenData(){
+         return {
+            nombre: this.nombre,
+           asignaturas: this.asignaturas
+         }
+    }
+}
+const estudiante1 = new Estudiante();
+console.log(estudiante1);
+console.log( estudiante1.obtenData());
