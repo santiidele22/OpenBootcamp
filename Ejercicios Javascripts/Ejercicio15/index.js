b6f19f2c37883d1f54f163e899f5fad79d54fc56
santiidele22 <ezedelebecq22@gmail.com>
2022-08-15 11:20:48 -0300

    first commit

---
+++ Ejercicios Javascripts/Ejercicio15/index.js
@@ -0,0 +1,10 @@
+const fibonacci = n => {
+    let fib = [0,1]
+   for(let i= 2; i <= n; i++){
+    fib[i] = fib[i - 1] + fib[i - 2]
+  
+   }
+   return fib[n]
+}
+console.log(fibonacci(6))
+console.log(acc);
